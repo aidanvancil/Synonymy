ef9984cbd76b3f2ebc79eb7a02e6e7f23187ccd3
aidanvancil <42700427+aidanvancil@users.noreply.github.com>
2023-04-05 20:15:33 -0700

    Patch

--- Synonymy/settings.py
+++ Synonymy/settings.py
@@ -32,7 +32,7 @@ STATICFILES_DIRS = [
     'game/static',
 ]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/game/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
